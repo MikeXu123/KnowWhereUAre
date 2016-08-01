0d49c2f61d5ac673c7bfcbb9f6f84ddcd776a854
sxc562586657 <sxc562586657@gmail.com>
2016-08-01 22:16:41 +0800

    Rename the package.

--- app/src/androidTest/java/com/sourcey/KnowWhereUAre/ApplicationTest.java
+++ app/src/androidTest/java/com/sourcey/KnowWhereUAre/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.sourcey.materiallogindemo;
+package com.sourcey.KnowWhereUAre;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/sourcey/KnowWhereUAre/MainActivity.java
+++ app/src/main/java/com/sourcey/KnowWhereUAre/MainActivity.java
@@ -1,4 +1,4 @@
-package com.sourcey.materiallogindemo;
+package com.sourcey.KnowWhereUAre;
 
 import android.content.Intent;
 import android.os.Bundle;
